f617e58a091b50d80cc72e367af5f4a9ab8a34fb
มะงิ้ง <102567759+cxGDN@users.noreply.github.com>
2024-11-11 17:06:22 +0000

    Added

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,35 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_child_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(1), 50)
 
+    def test_teens_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+    
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_elder_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(100), 100)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-10), "error")
+
+    def test_bva(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_bva2(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_bva3(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+
+
+        
 if __name__ == '__main__':
     unittest.main()
